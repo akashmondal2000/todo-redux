5aace7041e1340aa8cb7453375e8bb57a17a5e09
akashmondal2000 <mondalakash494@gmail.com>
2024-06-18 23:31:32 +0530

    config todos component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { removeTodo } from '../fetures/todo/todoSlice';
+
 
 const Todos = () => {
+
+  const todos = useSelector( state=> state.todos );
+
+  console.log(todos)
   return (
-    <div>Todos</div>
+    <div>
+      <ul>
+        {todos.map((todo)=>(
+          <li key={todo.id}> {todo.text}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
