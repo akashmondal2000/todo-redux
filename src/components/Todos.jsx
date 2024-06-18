af284f9022abebb0d4fbe19e28e3566a11f5a349
akashmondal2000 <mondalakash494@gmail.com>
2024-06-19 00:37:24 +0530

    todos style and remove todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { removeTodo } from '../fetures/todo/todoSlice';
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../fetures/todo/todoSlice";
 
 const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-  const todos = useSelector( state=> state.todos );
-
-  console.log(todos)
+  console.log(todos);
   return (
-    <div>
-      <ul>
-        {todos.map((todo)=>(
-          <li key={todo.id}> {todo.text}</li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+    <>
+      <div>
+        <h1 className=" text-[24px] font-bold font-sans">Todos</h1>
+        <ul className=" list-none flex flex-col justify-center items-center">
+          {/* show todo  */}
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded w-1/2"
+            >
+              <div className="text-white">{todo.text}</div>
+              {/*  */}
 
-export default Todos
+              <button onClick={() => dispatch(removeTodo(todo.id))}>
+                {/* image background round and center  */}
+                <div className="h-6 w-6 bg-white flex items-center justify-center rounded-[50%]">
+                  <img
+                    src="./images/delete.png"
+                    alt="delete-icon"
+                    className="h-4 w-4"
+                  />
+                </div>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Todos;
